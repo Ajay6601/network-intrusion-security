91a44c4e59752ed24e544777a97c9d07bef0b3fe
Ajay6601 <ajayreddy6601@gmail.com>
2024-11-23 02:11:26 -0500

    data validation

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ if __name__=='__main__':
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
-        print(dataingestionartifact)
         # logging.info("Data Initiation Completed")
         # print(dataingestionartifact)
         # data_validation_config=DataValidationConfig(trainingpipelineconfig)
