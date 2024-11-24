5456b325a42656d1f8e1451da36feedb4052b0cd
Ajay6601 <ajayreddy6601@gmail.com>
2024-11-24 14:46:42 -0500

    training model

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -12,6 +12,9 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
+
+SAVED_MODEL_DIR=os.path.join("saved_models")
+MODEL_FILE_NAME="model.pkl"
 """
 Data ingestion related constant start with DAT_INGESTION
 """
